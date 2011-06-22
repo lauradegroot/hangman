92881e95f1e79d90d5756495d9ab67a1fac511ca
Laura DeGroot <laurardegroot@gmail.com>
2011-06-22 11:53:46 -0400

    doin stuff

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,7 @@
 puts "What is your hangman word?"
-player_word = gets.chomp
-puts "you said #{player_word}!"
+player1_word = gets.chomp
+puts "you said #{player1_word}!"
 sleep 2
 system "clear"
+a = player1_word 
+puts "#{a.count}"
