91b6c4759f29c40d83325cb27b4a17cbabcd31e7
Laura DeGroot <laurardegroot@gmail.com>
2011-06-27 11:27:45 -0400

    things arent going so well

--- hangman.rb
+++ hangman.rb
@@ -1,18 +1,37 @@
 puts "Well hello there, welcome to hangman! Unfortunately you can't play hangman alone, so you should go find someone to play with. "
-sleep 3
+sleep 2
 puts "Good job. Now make sure they can't see the screen, because if they do, you might as well just be playing alone. What is your hangman word?"
 player1_word = gets.chomp
 puts "You said #{player1_word}"
-sleep 2
+sleep 1
 system "clear"
 puts"Go get the other guy and bring him here"
-sleep 3
+sleep 2
 puts "Hey guy, welcome. The word contains #{player1_word.length} letters. Guess a letter."
-guess = gets.chomp
-if player1_word.include? guess
+
+
+guessed_letter = gets.chomp 
+
+def guessing
+if player1_word.include? guessed_letter
     puts "Sweet! You're a genius. Guess again."
 else
     puts "Nope! Guess again."
 end
 
-("a".."z").to_a 
+
+guessed_letters = Array.new
+guessed_letters << guessed_letter
+while not game_over
+    if (guessed_letters.include guessed_letter)
+    puts :you_already_guessed_that_letter
+    next
+  end
+  if (guessed_letters!include guessed_letter)
+    puts :good_guess
+  elsif
+    puts :bad_guess
+  end
+  game_over = true if guessed_letters.count(10) 
+end
+end
