abde5300783b62a9705e6fd35d5250fb1345d14e
Laura DeGroot <laurardegroot@gmail.com>
2011-06-28 16:41:04 -0400

    trying to work out when a letter is wrong

--- hangman.rb
+++ hangman.rb
@@ -5,33 +5,40 @@ player1_word = gets.chomp
 puts "You said #{player1_word}"
 sleep 1
 system "clear"
-puts"Go get the other guy and bring him here"
+puts"Go get someone else and bring them here."
 sleep 2
-puts "Hey guy, welcome. The word contains #{player1_word.length} letters. Guess a letter."
-
-
+puts "Hey human, welcome. The word contains #{player1_word.length} letters. Guess a letter."
 
 
 max = 5
 guessed_letters = Array.new
+#wrong_letters = Array.new
 game_over = false
-
+ 
+ 
 while not game_over
   guessed_letter = gets.chomp 
   
-  
+    
     if (guessed_letters.include? guessed_letter)
-      puts "you're... dumb. you already guessed that. try again."
+      puts "You're... dumb. You already guessed that. Try again."
       next
   end
       guessed_letters << guessed_letter
+      #wrong_letters << 
     if (player1_word.include? guessed_letter)
-      puts "Nice, thats a letter in the word!"
-    else
+      puts "Nice, that's a letter in the word!"
+      
+      # guessed_letters.chars do |guessed_letter|
+      # puts guessed_letter + "_" 
+      # end
+    
+    if not (player1_word.include? guessed_letter) 
       puts "Nope"
     next
   end
   
 
-  game_over = true if guessed_letters.size > 5
+  
+  game_over = true if #wrong_letters.size >= 5
 end
