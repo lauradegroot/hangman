841ed1cb5f7871827b31517533a37460b209c56a
Laura DeGroot <laurardegroot@gmail.com>
2011-06-28 12:04:08 -0400

    things are happeningggg

--- hangman.rb
+++ hangman.rb
@@ -19,15 +19,19 @@ game_over = false
 while not game_over
   guessed_letter = gets.chomp 
   
+  
     if (guessed_letters.include? guessed_letter)
-      puts :you_already_guessed_that_letter
+      puts "you're... dumb. you already guessed that. try again."
       next
+  end
       guessed_letters << guessed_letter
+    if (player1_word.include? guessed_letter)
+      puts "Nice, thats a letter in the word!"
+    else
+      puts "Nope"
+    next
   end
-  if not (guessed_letters.include? guessed_letter)
-    puts :good_guess
-  elsif
-    puts :bad_guess
-  end
+  
+
   game_over = true if guessed_letters.size > 5
 end
